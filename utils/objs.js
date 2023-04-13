075de4c3a0d68df8f61b82d73d6fc2be424e0781
Faure1 <104220128+Faure1@users.noreply.github.com>
2023-04-13 10:19:18 -0300

    Update objs.js

--- utils/objs.js
+++ utils/objs.js
@@ -1,28 +1,23 @@
 export const educacion = [
     { id: "1", text: "Estoy por terminar la seundaria " },
-    { id: "2", text: "Participe de diversos modelos de nacion unidas (MNU) donde conoci mucha gente y aprendi mucho" },
-    { id: "3", text: "Aprendi programacion junto a dos compañeros mas" },
+    { id: "2", text: "Participe de diversos proyectos de progrmación" },
+    { id: "3", text: "Arme un grupo de programacion con otros 2 compñeros" },
     { id: "4", text: "Participe en proyecto Zondar" },
 ];
 
 export const favoritos = [
-    { id: "1", text: "Tocar la guitarra" },
-    { id: "2", text: "Ir al gimnadio" },
+    { id: "1", text: "Se mucho de futbol" },
+    { id: "2", text: "Jugar a los juegos de pc" },
     { id: "3", text: "Programar" },
-    { id: "4", text: "Estar con mi novia" },
+    { id: "4", text: "Jugar al fitbol" },
     { id: "5", text: "Juntarme con mis amigos" },
-    { id: "6", text: "Andar en bici" },
+    { id: "6", text: "Estar con mis mascotas" },
 ];
 
 export const habilidades = [
-    { id: "1", text: "Tocar la guitarra profesionalmente" },
+    { id: "1", text: "Soy bueno en las ciencias exactas" },
     { id: "3", text: "Programar" },
-    { id: "5", text: "Tener buena oratoria" },
+    { id: "5", text: "Aprendo rapido a todo tipo de nueva tecnologia" },
 ];
-
-
-
-
-
 
 
